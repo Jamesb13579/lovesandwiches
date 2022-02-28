3598843f659ddf0a0e030393040c5c74c3ca72f4
Jamesb13579 <jamesbagge@ymail.com>
2022-02-28 05:21:29 +0000

    Add sales data to sales work sheet

--- run.py
+++ run.py
@@ -46,6 +46,20 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
+
     return True
 
+
+def update_sales_worksheet(data):
+    """.git/update sales worksheet, add new row with list data provided.
+    """
+    
+    print("updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales work sheet updated successfully.\n")
+
+ 
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
